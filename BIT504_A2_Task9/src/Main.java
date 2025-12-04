567fcec86510c99d3e54984f8b15f9dcd5d61741
kristen-de-leon <kristenssen05@gmail.com>
2025-12-04 21:35:33 +1300

    Initial commit

---
+++ BIT504_A2_Task9/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello!");
+		System.out.println("This is Task 9 of BIT504 Assessment 2");
+
+	}
+
+}
